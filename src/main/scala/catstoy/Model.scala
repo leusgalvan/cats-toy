7fd94f5bee9aed0bffb342c9501218371f936f84
Leandro Galvan <leusgalvan@gmail.com>
2020-03-06 22:44:18 -0300

    Improve serializer

--- src/main/scala/catstoy/Model.scala
+++ src/main/scala/catstoy/Model.scala
@@ -1,9 +1,26 @@
 package catstoy
 
+import java.nio.ByteBuffer
+
 object Model {
   case class User(username: String, password: String, email: String)
 
+  val userMarker = 3.toByte
   implicit val userSerialInstance = Serial.instance[User] { user =>
-    Serial[List[String]].serialize(List(user.username, user.email, user.password))
+    val bytes = Serial[List[String]].serialize(List(user.username, user.email, user.password))
+    Array(userMarker, bytes.length.toByte) ++
+      Serial[String].serialize(user.username) ++
+      Serial[String].serialize(user.password) ++
+      Serial[String].serialize(user.email)
   }
+  {
+    case bs if bs.length <= 1 || bs.head != userMarker => None
+    case bs =>
+      for {
+        username <- Serial[String].deserialize(bs.drop(2))
+        password <- Serial[String].deserialize(bs.drop(4 + username.length))
+        email    <- Serial[String].deserialize(bs.drop(6 + username.length + password.length))
+      } yield User(username, password, email)
+  }
+
 }
--- src/main/scala/catstoy/Serial.scala
+++ src/main/scala/catstoy/Serial.scala
@@ -1,13 +1,52 @@
 package catstoy
 
+import java.nio.ByteBuffer
+
+import scala.annotation.tailrec
+
 trait Serial[A] {
   def serialize(a: A): Array[Byte]
+  def deserialize(bytes: Array[Byte]): Option[A]
 }
 
 object Serial{
   def apply[A](implicit serial: Serial[A]): Serial[A] = serial
-  def instance[A](func: A => Array[Byte]): Serial[A] = (a: A) => func(a)
-  implicit val stringSerialInstance = instance[String](_.toCharArray.map(_.toByte))
-  implicit def listSerialInstance[A](implicit serialA: Serial[A]): Serial[List[A]] =
-    instance[List[A]](_.foldLeft(Array[Byte]()){case (bytes, a) => bytes ++ Serial[A].serialize(a)})
+  def instance[A](s: A => Array[Byte])(d: Array[Byte] => Option[A]): Serial[A] = new Serial[A] {
+    override def serialize(a: A): Array[Byte] = s(a)
+    override def deserialize(bytes: Array[Byte]): Option[A] = d(bytes)
+  }
+
+  val stringMarker = 1.toByte
+  implicit val stringSerialInstance = instance[String]{ s =>
+    val bytes = s.toCharArray.map(_.toByte)
+    Array(stringMarker, bytes.length.toByte) ++ bytes
+  } {
+    case bs if bs.length <= 1 || bs.head != stringMarker => None
+    case bs => Some(new String(bs.slice(2, 2 + bs(1)).map(_.toChar)))
+  }
+
+  val listMarker = 2.toByte
+  implicit def listSerialInstance[A](implicit serialA: Serial[A]): Serial[List[A]] = {
+    @tailrec
+    def read(bytes: Array[Byte], start: Int, readSoFar: List[A]): Option[List[A]] = {
+      if(start >= bytes.length) {
+        Some(readSoFar.reverse)
+      }
+      else {
+        val end = start + bytes(1) + 2
+        serialA.deserialize(bytes.drop(start)) match {
+          case None => None
+          case Some(next) => read(bytes, end, next :: readSoFar)
+        }
+      }
+    }
+
+    instance[List[A]] { xs =>
+      val bytes = xs.foldLeft(Array[Byte]()) { case (bytes, a) => bytes ++ Serial[A].serialize(a) }
+      Array(listMarker, bytes.length.toByte) ++ bytes
+    } {
+      case bs if bs.length <= 1 || bs.head != listMarker => None
+      case bs => read(bs, 2, Nil)
+    }
+  }
 }
